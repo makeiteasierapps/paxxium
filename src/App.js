fbc3521165b0b8fc32956c5af18d583b30757d78
eccos <eccos@users.noreply.github.com>
2023-12-01 21:39:11 -0800

    WIP: Add routing to Nav tabs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from "react";
 import {
     BrowserRouter as Router,
     Route,
@@ -6,21 +6,28 @@ import {
     useNavigate,
     Navigate,
     useLocation,
-} from 'react-router-dom';
-import LoginPage from './components/auth/LoginPage';
-import SignUpPage from './components/auth/SignUpPage';
-import './styles/App.css';
-import MainDash from './components/dashboards/main/MainDash';
-import CssBaseline from '@mui/material/CssBaseline';
-import { AuthProvider, AuthContext } from './contexts/AuthContext';
-import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './Theme';
-import TitleBar from './components/dashboards/main/AppBar';
-import { getFirestore, doc, getDoc } from 'firebase/firestore';
+} from "react-router-dom";
+import LoginPage from "./components/auth/LoginPage";
+import SignUpPage from "./components/auth/SignUpPage";
+import "./styles/App.css";
+import MainDash from "./components/dashboards/main/MainDash";
+import CssBaseline from "@mui/material/CssBaseline";
+import { AuthProvider, AuthContext } from "./contexts/AuthContext";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./Theme";
+import TitleBar from "./components/dashboards/main/AppBar";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import ChatDashboard from "./components/dashboards/chat/ChatDashboard";
+import Home from "./components/dashboards/home/Home";
+import Profile from "./components/dashboards/profile/Profile";
+import { ChatProvider } from "./contexts/ChatContext";
+import { NewsProvider } from "./contexts/NewsContext";
+import { ProfileProvider } from "./contexts/ProfileContext";
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const AuthenticatedApp = () => {
+    const [lastVisitedRoute, setLastVisitedRoute] = useState("");
     const {
         idToken,
         uid,
@@ -35,15 +42,19 @@ const AuthenticatedApp = () => {
     const navigate = useNavigate();
     const location = useLocation();
 
-    useEffect(() => {
-        if (location.pathname !== '/signup') {
-            if (!idToken) {
-                navigate('/');
-            } else if (isAuthorized) {
-                navigate('/dashboard');
-            }
-        }
-    }, [idToken, isAuthorized, navigate, location.pathname]);
+    // useEffect(() => {
+    //     // Get the last visited route from storage on initial load
+    //     const lastRoute = localStorage.getItem("lastVisitedRoute");
+    //     if (lastRoute) {
+    //         setLastVisitedRoute(lastRoute);
+    //     }
+    // }, []);
+
+    // useEffect(() => {
+    //     // Update lastVisitedRoute in storage when location changes
+    //     setLastVisitedRoute(location.pathname);
+    //     localStorage.setItem("lastVisitedRoute", location.pathname);
+    // }, [location.pathname]);
 
     // Fetches auth status from the db then loads the user into state.
     useEffect(() => {
@@ -51,9 +62,9 @@ const AuthenticatedApp = () => {
             if (idToken && user) {
                 try {
                     const response = await fetch(`${backendUrl}/auth_check`, {
-                        method: 'POST',
+                        method: "POST",
                         headers: {
-                            'Content-Type': 'application/json',
+                            "Content-Type": "application/json",
                             Authorization: idToken,
                         },
                         body: JSON.stringify({
@@ -67,14 +78,14 @@ const AuthenticatedApp = () => {
                     if (responseData.auth_status) {
                         setIsAuthorized(true);
                         setUid(user.uid);
-                        const userDoc = await getDoc(doc(db, 'users', uid));
+                        const userDoc = await getDoc(doc(db, "users", uid));
                         if (!userDoc.exists()) {
-                            throw new Error('No user found in Firestore');
+                            throw new Error("No user found in Firestore");
                         }
                         setUsername(userDoc.data().username);
                     }
                 } catch (error) {
-                    console.error('Failed to fetch:', error);
+                    console.error("Failed to fetch:", error);
                 }
             }
         };
@@ -88,17 +99,60 @@ const AuthenticatedApp = () => {
             <Routes>
                 {isAuthorized ? (
                     <>
-                        <Route path="/dashboard" element={<MainDash />} />
                         <Route
-                            path="*"
-                            element={<Navigate replace to="/dashboard" />}
+                            path="/"
+                            element={
+                                <Navigate
+                                    replace
+                                    to={lastVisitedRoute || "/home"}
+                                />
+                            }
+                        />
+                        <Route
+                            path="/dashboard"
+                            element={
+                                <Navigate
+                                    replace
+                                    to={lastVisitedRoute || "/home"}
+                                />
+                            }
+                        />
+                        <Route
+                            path="/home"
+                            element={
+                                <MainDash>
+                                    <NewsProvider>
+                                        <Home />
+                                    </NewsProvider>
+                                </MainDash>
+                            }
+                        />
+                        <Route
+                            path="/agents"
+                            element={
+                                <MainDash>
+                                    <ChatProvider>
+                                        <ChatDashboard />
+                                    </ChatProvider>
+                                </MainDash>
+                            }
+                        />
+                        <Route
+                            path="/profile"
+                            element={
+                                <MainDash>
+                                    <ProfileProvider>
+                                        <Profile />
+                                    </ProfileProvider>
+                                </MainDash>
+                            }
                         />
                     </>
                 ) : (
                     <>
                         <Route path="/" element={<LoginPage />} />
-                        <Route path="/signup" element={<SignUpPage />} />
                         <Route path="*" element={<Navigate replace to="/" />} />
+                        <Route path="/signup" element={<SignUpPage />} />
                     </>
                 )}
             </Routes>
--- src/components/dashboards/main/NavTabs.js
+++ src/components/dashboards/main/NavTabs.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { Tabs, Tab, Box, styled } from '@mui/material';
+import { useState } from "react";
+import { Tabs, Tab, Box, styled } from "@mui/material";
+import { Link } from "react-router-dom";
 
 // Function to generate accessibility properties for tabs
 // This function provides flexibility, reduces errors, promotes code reusability, and improves readability
@@ -8,37 +9,37 @@ function a11yProps(index) {
     // These properties are used by screen readers and other assistive technologies
     return {
         id: `simple-tab-${index}`, // unique id for each tab
-        'aria-controls': `simple-tabpanel-${index}`, // id of the element controlled by this tab
+        "aria-controls": `simple-tabpanel-${index}`, // id of the element controlled by this tab
     };
 }
 
 // Styled Components
 const StyledNavTabs = styled(Tabs)(({ theme }) => ({
-    position: 'fixed',
-    top: '48px', // replace '64px' with the height of your AppBar
+    position: "fixed",
+    top: "48px", // replace '64px' with the height of your AppBar
     zIndex: theme.zIndex.appBar,
-    width: '100%',
+    width: "100%",
     backgroundColor: theme.palette.background.default,
 }));
 
 // Define NavTabs component
-const NavTabs = ({ setValue }) => {
+const NavTabs = () => {
     // Use React's useState hook to manage the selected tab index
     const [index, setIndex] = useState(0);
 
     // Render the NavTabs component
     return (
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <StyledNavTabs
                 value={index}
                 onChange={(_, index) => {
                     setIndex(index);
-                    setValue(index);
+                    // setValue(index);
                 }}
                 sx={{
                     // Remove the tab indicator
-                    '& .MuiTabs-indicator': {
-                        display: 'none',
+                    "& .MuiTabs-indicator": {
+                        display: "none",
                     },
                 }}
                 aria-label="navigation tabs"
@@ -46,9 +47,24 @@ const NavTabs = ({ setValue }) => {
                 textColor="secondary"
             >
                 {/* Use the a11yProps function to generate unique id and aria-controls values for each tab */}
-                <Tab label="Home" {...a11yProps(0)} />
-                <Tab label="Agents" {...a11yProps(1)} />
-                <Tab label="Profile" {...a11yProps(2)} />
+                <Tab
+                    label="Home"
+                    {...a11yProps(0)}
+                    component={Link}
+                    to="/home"
+                />
+                <Tab
+                    label="Agents"
+                    {...a11yProps(1)}
+                    component={Link}
+                    to="/agents"
+                />
+                <Tab
+                    label="Profile"
+                    {...a11yProps(2)}
+                    component={Link}
+                    to="/profile"
+                />
             </StyledNavTabs>
         </Box>
     );
