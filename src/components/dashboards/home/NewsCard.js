21df90eb47ddd3b698967b52920d16d35a9e7ce1
eccos <eccos@users.noreply.github.com>
2023-12-07 17:39:34 -0800

    fix motion.div H. edit card size px to %s

--- src/components/dashboards/home/NewsCard.js
+++ src/components/dashboards/home/NewsCard.js
@@ -1,21 +1,21 @@
-import { useContext, useState } from 'react';
-import { AuthContext } from '../../../contexts/AuthContext';
-import { NewsContext } from '../../../contexts/NewsContext';
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import DeleteIcon from "@mui/icons-material/Delete";
 import {
-    Card,
-    CardMedia,
-    CardContent,
-    Typography,
-    CardActions,
     Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
     IconButton,
     Tooltip,
-} from '@mui/material';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { motion } from 'framer-motion';
-import DeleteConfirmationDialog from './DeleteConfirmationDialog';
+    Typography,
+} from "@mui/material";
+import { motion } from "framer-motion";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { NewsContext } from "../../../contexts/NewsContext";
+import DeleteConfirmationDialog from "./DeleteConfirmationDialog";
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
@@ -28,9 +28,9 @@ const NewsCard = ({ news, index }) => {
     const markArticleRead = async () => {
         try {
             const response = await fetch(`${backendUrl}/news_articles`, {
-                method: 'PUT',
+                method: "PUT",
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                     Authorization: idToken,
                 },
                 body: JSON.stringify({ articleId: news.id }),
@@ -48,9 +48,9 @@ const NewsCard = ({ news, index }) => {
     const handleArticleDelete = async () => {
         try {
             const response = await fetch(`${backendUrl}/news_articles`, {
-                method: 'DELETE',
+                method: "DELETE",
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                     Authorization: idToken,
                 },
                 body: JSON.stringify({ articleId: news.id }),
@@ -70,30 +70,29 @@ const NewsCard = ({ news, index }) => {
             initial={{ opacity: 0, scale: 0.3 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.9 }}
-            height={'100vh'}
+            style={{ height: "100%" }}
         >
             <Card
                 key={news.id}
                 sx={{
-                    height: 600,
-                    width: 400,
-                    overflow: 'scroll',
-                    boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)',
+                    height: "100%",
+                    overflowY: "scroll",
+                    boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
                 }}
                 onClick={() => setSlideIndex(index)}
             >
                 <CardMedia
-                    sx={{ height: 200, position: 'relative' }}
+                    sx={{ height: "40%", position: "relative" }}
                     image={news.image}
                 >
                     <Tooltip title="Mark read" placement="top-end">
                         <IconButton
                             sx={{
-                                position: 'absolute',
+                                position: "absolute",
                                 top: 0,
                                 right: 0,
                                 opacity: 0.3,
-                                '&:hover': { opacity: 1 },
+                                "&:hover": { opacity: 1 },
                             }}
                             onClick={(e) => {
                                 e.stopPropagation(); // Prevent triggering Card's onClick
@@ -112,11 +111,11 @@ const NewsCard = ({ news, index }) => {
                     <Tooltip title="Trash" placement="top-start">
                         <IconButton
                             sx={{
-                                position: 'absolute',
+                                position: "absolute",
                                 top: 0,
                                 left: 0,
                                 opacity: 0.3,
-                                '&:hover': { opacity: 1 },
+                                "&:hover": { opacity: 1 },
                             }}
                             onClick={(e) => {
                                 e.stopPropagation();
@@ -127,7 +126,7 @@ const NewsCard = ({ news, index }) => {
                         </IconButton>
                     </Tooltip>
                 </CardMedia>
-                <CardContent>
+                <CardContent sx={{ height: "60%" }}>
                     <Typography variant="h5" component="div" gutterBottom>
                         {news.title}
                     </Typography>
